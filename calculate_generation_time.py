1d5faab376b0ae995bb0fa4345d9b39188b5c77f
油腻中青年 <82805987+cxl-xuaner@users.noreply.github.com>
2024-10-10 15:08:03 +0800

    Update and rename 20241009-作业1-计算生成指定特征sha256hash耗时.py to calculate_generation_time.py
    20241009_01-calculation of sha256hash generation time

--- calculate_generation_time.py
+++ calculate_generation_time.py
@@ -3,8 +3,7 @@ import random
 import datetime
 
 # 计算生成指定0数量开头的SHA256Hash结果需要花费的时间
-#计算耗时
-def calculate_time_cost(func,my_wechat_alias,length):
+def calculate_time_cost(func,*args):
     """
     计算函数`func`的耗时。
     :param func: 要测量耗时的函数
@@ -13,13 +12,18 @@ def calculate_time_cost(func,my_wechat_alias,length):
     print("*" * 50, "开始执行", "*" * 50)
     print(f'计算{length}个0开头hash耗时：')
     start_time = datetime.datetime.now()
-    func(my_wechat_alias,length)
+    func(args)
     end_time = datetime.datetime.now()
     elapsed_time = end_time - start_time
     cost_seconds = elapsed_time.total_seconds()
     print("*" * 50, "执行完毕,累计耗时%s秒" % cost_seconds, "*" * 50)
 
-def calculate_head(my_wechat_alias,length):
+def calculate_head(args):
+    """
+    
+    """
+    my_wechat_alias = args[0]
+    length = args[1]
     sha256_hash = hashlib.sha256()
     nonce = 0
     while True:
